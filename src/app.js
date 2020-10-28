c654603173e493a6f45166a8e70951bcd07e6bd5
Sandro Valgoi <sandrovalgoi@gmail.com>
2020-10-27 22:47:19 -0300

    solução desafio 1 - conceitos node

--- src/app.js
+++ src/app.js
@@ -1,33 +1,58 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const checkValidId = (request, response, next) => {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid Uuid Id!" });
+  }
+  const index = repositories.findIndex(repository => repository.id === id);
+  if (index < 0) {
+    return response.status(404).json({ error: "Repository not found!" });
+  }
+  request.body.index = index;
+  return next();
+};
+
+app.use('/repositories/:id', checkValidId);
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { index } = request.body;
+  const { title, url, techs } = request.body;
+  const repository = {...repositories[index], title, url, techs};
+  repositories[index] = repository;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { index } = request.body;
+  repositories.splice(index, 1);
+  return response.status(204).json({success: "Repository wiped out!"})
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { index } = request.body;
+  repositories[index].likes++;
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
